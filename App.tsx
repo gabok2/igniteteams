992c7480566c4d0d2cafa0fec6e0ec16d1eddd59
unknown <gabrielbld9@gmail.com>
2023-01-30 00:07:06 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+import { ThemeProvider } from 'styled-components/native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import theme from './src/theme';
+
+import { Loading } from '@components/Loading';
+import { NewGroup } from '@screens/NewGroup';
+import { Groups } from '@screens/Groups';
+import { Players } from '@screens/Players';
+import { Routes } from './src/routes';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: theme.COLORS.GRAY_600 }}>
+      <ThemeProvider theme={theme}>
+        <>
+          <StatusBar
+            barStyle="light-content"
+            backgroundColor="transparent"
+            translucent
+          />
+          {fontsLoaded ? <Routes /> : <Loading />}
+        </>
+      </ThemeProvider>
+    </SafeAreaView>
+
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
